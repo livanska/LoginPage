7cd8549ed50def4d661c87e6e4e3c4d06e5ee7f2
Lilia16012001 <ivanskalilia16@gmail.com>
2021-02-26 00:26:26 +0200

    Created page ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <div className='container-page-content'>
+        <div className='container-image'>
+          <img className='image' src='https://wallpaperaccess.com/full/1545922.jpg'></img>
+        </div>
+        <div className='container-logging-form'>
+          <div className='logging-form-content'>
+            <div className='headers'>
+              <h2 id='title'>Log in</h2>
+              <h4 id='subtitle'>
+                If you don`t have an account yet, please <a href=''>register</a> first.
+              </h4>
+            </div>
+            <div className='inputs'>
+              <input className='form-input' placeholder='Your e-mail'></input>
+              <input className='form-input' placeholder='Your password'></input>
+              <a href=''>I forgot my password</a>
+            </div>
+            <div className='container-checkbox'>
+              <label htmlFor='custom-input' className='checkbox'>
+                <input id='custom-input' type='checkbox'></input>
+                <span className='checkmark'></span>
+                <p>Remember me</p>
+              </label>
+            </div>
+            <button className='button-primary'>Log in</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
